1d0645368f2a18049a5855daa5723ef20001637d
cssilva99 <claudia.sofia.msilva@gmail.com>
2025-04-08 13:09:01 +0100

    policy

--- main.tf
+++ main.tf
@@ -1,10 +1,33 @@
+resource "aws_sns_topic" "topic" {
+  name = "s3-event-notification-topic"
+
+  policy = <<POLICY
+{
+    "Version":"2012-10-17",
+    "Statement":[{
+        "Effect": "Allow",
+        "Principal": {"AWS":"*"},
+        "Action": "SNS:Publish",
+        "Resource": "arn:aws:sns:*:*:s3-event-notification-topic",
+        "Condition":{
+            "ArnLike":{"aws:SourceArn":"${aws_s3_bucket.bucket.arn}"}
+        }
+    }]
+}
+POLICY
+}
+
+
 resource "aws_s3_bucket" "my_objects" {
   bucket = "ourcorp"
 }
 
-resource "aws_s3_bucket_public_access_block" "inventory" {
+resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = "${aws_s3_bucket.my_objects.id}"
 
-  block_public_acls   = true
-  block_public_policy = true
+  topic {
+    topic_arn     = "${aws_sns_topic.topic.arn}"
+    events        = ["s3:ObjectCreated:*"]
+    filter_suffix = ".log"
+  }
 }

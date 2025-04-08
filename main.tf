f957363ae4814c3b9a31ea49b4260183c8a5eef4
cssilva99 <claudia.sofia.msilva@gmail.com>
2025-04-08 10:44:04 +0100

    Added a provider

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = var.region
-}
-
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket-1234"
    
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
   type        = string
   default     = "eu-west-2"
-  description = "If specified, the AWS region this bucket should reside in. Otherwise, the region used by the callee"
+  description = ""
 }

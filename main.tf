1c4641166ab3570ce392db3c513ed3cd6d156180
cssilva99 <claudia.sofia.msilva@gmail.com>
2025-04-08 12:56:50 +0100

    process

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "example"
+resource "aws_s3_bucket" "my_objects" {
+  bucket = "ourcorp"
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = "${aws_s3_bucket.example.id}"
+resource "aws_s3_bucket_public_access_block" "inventory" {
+  bucket = "${aws_s3_bucket.my_objects.id}"
 
   block_public_acls   = true
   block_public_policy = true

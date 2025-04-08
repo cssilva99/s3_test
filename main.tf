38a8ab79329bb8cf23df027dc57a90563e7954ac
cssilva99 <claudia.sofia.msilva@gmail.com>
2025-04-08 10:26:55 +0100

    Added a diferent s3 bucket

--- main.tf
+++ main.tf
@@ -1,28 +1,11 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# SPDX-License-Identifier: MIT-0
-
-provider "aws" {
-  region = module.environment.aws_region
-}
-
-terraform {
-  backend "s3" {
-    region  = "us-east-1"
-    bucket  = "<YOUR BUCKET NAME GOES HERE>"
-    key     = "environments/providers/aws/prd/infraestructure/vpc/main.tf"
-    encrypt = true
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-1234"
+  acl    = "private"
+  region   = "us-east-1"
+   
+  tags = {
+    Name        = "My S3 test bucket"
+    Environment = "Sandbox"
+    Owner       = "Narendra Yala",
+    DataType    = "Test files"  
   }
-}
-
-//Import the constants
-module "environment" {
-  source = "../../"
-}
-
-module "default_vpc" {
-  source      = "../../../../../../modules/providers/aws/vpc"
-  vpc_name    = var.vpc_name
-  cidr_vpc    = var.cidr_vpc
-  environment = "prd"
-}

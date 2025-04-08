887a519f9b2798abb176f476f1f3693184fb6bc9
cssilva99 <claudia.sofia.msilva@gmail.com>
2025-04-08 10:33:32 +0100

    Removed acl

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket-1234"
-  acl    = "private"
-  region   = "us-east-1"
+  region   = "us-west-2"
    
   tags = {
     Name        = "My S3 test bucket"

1df6ce43537fe9ad056160e86a82117b0b1b1efa
cssilva99 <claudia.sofia.msilva@gmail.com>
2025-04-08 12:33:25 +0100

    Added 2 buckets

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
+resource "aws_s3_bucket" "originbucket" {
+  bucket = "logreceive"
+}
+
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "logdelivery"
 
   logging {
-    target_bucket = "${aws_s3_bucket.log_bucket.id}"
+    target_bucket = "${aws_s3_bucket.originbucket.id}"
     target_prefix = "log/"
   }
 }

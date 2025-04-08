caa5751b971cf30029b891315327c47d56399c09
cssilva99 <claudia.sofia.msilva@gmail.com>
2025-04-08 12:48:41 +0100

    peaks

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "sourcebusdfsdfdsfsdcket" {
   bucket = "sourcebusdfsdfdsfsdcket"
 
   logging {
-    target_bucket = "${aws_s3_bucket.bucket.id}"
+    target_bucket = "${aws_s3_bucket.sadsf.id}"
     target_prefix = "log/"
   }
 }
